890f6e1c77cb8c111edced65a1673d39d20d02f6
MikhailLeonov <mikhail.leonov>
2023-02-26 19:04:53 +0300

    added test.php script

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+ob_start();
+
+session_start();
+$_SESSION['config'] = parse_ini_file("config.ini", true)['localhost'];
+
+require_once('db/AwsUsersData.php');
+
+$test = new AwsUsersData();
+
+
+?>
